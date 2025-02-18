51a15533214adfcd5987c1efc13660790e846c13
Amaury GODOT <160280641+Amzu-bzh@users.noreply.github.com>
2025-02-18 20:54:36 +0100

    Add integration of Led and Board class

--- arduinoSNT/led.py
+++ arduinoSNT/led.py
@@ -1,11 +1,18 @@
 import pyfirmata
 
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .board import Board
+
 class Led:
-    def __init__(self, board, pin: int) -> None:
+    def __init__(self, board: "Board", pin: int) -> None:
         try:
             self.pin = pin
             self.led = board.board.digital[pin]
             self.led.mode = pyfirmata.OUTPUT
+
+            board.add_led(self)
+
         except Exception as e:
             print(f"An error as occurred during the instantiation of the class Led:\n{e}")
             quit()
