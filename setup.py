c52e5ecb7c0b9e354cd3d0cdc252629521a3fcce
Amaury GODOT <160280641+Amzu-bzh@users.noreply.github.com>
2025-02-18 20:17:55 +0100

    Upload new project version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="arduino-snt",
-    version='1.0.1',
+    version='1.1.0',
     packages=find_packages(),
     install_requires=[
         "pyfirmata>=1.1.0"
